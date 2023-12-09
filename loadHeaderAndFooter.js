e1ed93bfb60902767a2a84f0050fbcfdf99989cd
noaleclaire <noa.leclaire@epitech.eu>
2023-12-09 13:25:06 +0100

    [+]: faculty page

--- loadHeaderAndFooter.js
+++ loadHeaderAndFooter.js
@@ -11,6 +11,22 @@ fetch('/header/header.html')
     var parent = mainContent.parentNode;
 
     parent.insertBefore(header, mainContent);
+    switch (document.title) {
+      case "College of Engineering - Ahlia University | Bahrain":
+        document.getElementById('title-logo').lastElementChild.innerHTML = "College of Engineering"
+        break;
+      case "College of Engineering - Faculty | Bahrain":
+        document.getElementById('title-logo').lastElementChild.innerHTML = "Faculty"
+        break;
+      case "College of Engineering - Programmes | Bahrain":
+        document.getElementById('title-logo').lastElementChild.innerHTML = "Programmes"
+        break;
+      case "College of Engineering - Events | Bahrain":
+        document.getElementById('title-logo').lastElementChild.innerHTML = "Events"
+        break;
+      default:
+        break;
+    }
   })
   .catch(error => {
     console.log('An error occured when loading the header: ', error);
